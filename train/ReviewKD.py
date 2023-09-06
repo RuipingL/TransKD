39ab795d37d32bf2638acf9a384754cd25f7fd98
RuipingL <65483202+RuipingL@users.noreply.github.com>
2023-09-06 12:28:00 +0200

    Add files via upload

---
+++ train/ReviewKD.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from models.Segformer_rliu import mit_b0,mit_b1,mit_b2#,mit_b3,mit_b4,mit_b5
+class ABF(nn.Module):
+    def __init__(self, in_channel, mid_channel, out_channel, fuse):
+        super(ABF, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channel, mid_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(mid_channel),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(mid_channel, out_channel,kernel_size=3,stride=1,padding=1,bias=False),
+            nn.BatchNorm2d(out_channel),
+        )
+        if fuse:
+            self.att_conv = nn.Sequential(
+                    nn.Conv2d(mid_channel*2, 2, kernel_size=1),
+                    nn.Sigmoid(),
+                )
+        else:
+            self.att_conv = None
+        nn.init.kaiming_uniform_(self.conv1[0].weight, a=1)  # pyre-ignore
+        nn.init.kaiming_uniform_(self.conv2[0].weight, a=1)  # pyre-ignore
+
+    def forward(self, x, y=None, shape=None):
+        n,_,h,w = x.shape
+        # transform student features
+        x = self.conv1(x)
+        if self.att_conv is not None:
+            # upsample residual features
+            shape = x.shape[-2:]
+            y = F.interpolate(y, shape, mode="nearest")
+            # fusion
+            z = torch.cat([x, y], dim=1)
+            z = self.att_conv(z)
+            x = (x * z[:,0].view(n,1,h,w) + y * z[:,1].view(n,1,h,w))
+        # output 
+        y = self.conv2(x)
+        return y, x
+
+class ReviewKD(nn.Module):
+    def __init__(
+        self,student, in_channels, out_channels, mid_channel
+    ):
+        super(ReviewKD, self).__init__()
+        self.student = student
+
+        abfs = nn.ModuleList()
+
+        for idx, in_channel in enumerate(in_channels):
+            abfs.append(ABF(in_channel, mid_channel, out_channels[idx], idx < len(in_channels)-1))
+
+
+        self.abfs = abfs[::-1]
+
+    def forward(self, x):
+        student_features = self.student(x,is_feat=True)
+        logit = student_features[1]
+        x = student_features[0][::-1]
+        results = []
+        out_features, res_features = self.abfs[0](x[0])
+        results.append(out_features)
+        for features, abf in zip(x[1:], self.abfs[1:]):
+            out_features, res_features = abf(features, res_features)
+            results.insert(0, out_features) 
+
+        return results, logit
+
+
+def build_kd_trans(model,in_channels = [32, 64, 160, 256], out_channels = [64, 128, 320, 512]):# in_channels: Segformer B0, out_channels: Segformer B2
+    mid_channel = 64
+    student = model
+    model = ReviewKD(student, in_channels, out_channels, mid_channel)
+    return model
+
+def hcl(fstudent, fteacher):
+    loss_all = 0.0
+    for fs, ft in zip(fstudent, fteacher):
+        n,c,h,w = fs.shape
+        loss = F.mse_loss(fs, ft, reduction='mean')
+        cnt = 1.0
+        tot = 1.0
+        for l in [4,2,1]:
+            if l >=h:
+                continue
+            tmpfs = F.adaptive_avg_pool2d(fs, (l,l))
+            tmpft = F.adaptive_avg_pool2d(ft, (l,l))
+            cnt /= 2.0
+            loss += F.mse_loss(tmpfs, tmpft, reduction='mean') * cnt
+            tot += cnt
+        loss = loss / tot
+        loss_all = loss_all + loss
+    return loss_all
